bcef1dfe44acfedc8cf88094a1fd510a38e2422f
gaohanghang <1341947277@qq.com>
2019-09-04 00:17:38 +0800

    update

--- src/main/java/org/hackerandpainter/springbootsampleurlshortener/controller/UrlShortenerController.java
+++ src/main/java/org/hackerandpainter/springbootsampleurlshortener/controller/UrlShortenerController.java
@@ -4,7 +4,6 @@ import com.google.common.hash.Hashing;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.hackerandpainter.springbootsampleurlshortener.InvalidUrlException;
-import org.hibernate.validator.internal.constraintvalidators.hv.URLValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
